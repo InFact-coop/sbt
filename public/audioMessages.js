441ccef97d1af52fcfb9022bfbbfdc80e1edfb7f
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-11-26 11:24:05 +0000

    fixes audio recording

--- public/audioMessages.js
+++ public/audioMessages.js
@@ -1,4 +1,3 @@
-
 if (navigator.mediaDevices) {
   console.log('getUserMedia supported.');
   console.log('App: ', app);
@@ -7,36 +6,39 @@ if (navigator.mediaDevices) {
     audio: true
   };
   var chunks = [];
+  var mediaRecorder;
 
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    var mediaRecorder = new MediaRecorder(stream);
-
-    app.ports.recordStart.subscribe(function() {
+  app.ports.recordStart.subscribe(function() {
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+      mediaRecorder = new MediaRecorder(stream);
       mediaRecorder.start();
       console.log(mediaRecorder.state);
       console.log("recorder started");
-    });
 
-    app.ports.recordStop.subscribe(function() {
+      mediaRecorder.onstop = function(e) {
+        console.log("data available after MediaRecorder.stop() called.");
+
+        var blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
+        chunks = [];
+        // jstoelm
+        var audioURL = window.URL.createObjectURL(blob);
+        app.ports.audioUrl.send(audioURL);
+        console.log('AudioURL: ', audioURL);
+        console.log("recorder stopped");
+      };
+
+      mediaRecorder.ondataavailable = function(e) {
+        chunks.push(e.data);
+      };
+    })
+  });
+
+  app.ports.recordStop.subscribe(function() {
+    if (mediaRecorder) {
       mediaRecorder.stop();
-      console.log(mediaRecorder.state);
-      console.log("recorder stopped");
-    });
+    }
+    console.log(mediaRecorder.state);
+    console.log("recorder stopped");
+  });
 
-    mediaRecorder.onstop = function(e) {
-      console.log("data available after MediaRecorder.stop() called.");
-
-      var blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
-      chunks = [];
-      // jstoelm
-      var audioURL = window.URL.createObjectURL(blob);
-      app.ports.audioUrl.send(audioURL);
-      console.log('AudioURL: ', typeof audioURL);
-      console.log("recorder stopped");
-    };
-
-    mediaRecorder.ondataavailable = function(e) {
-      chunks.push(e.data);
-    };
-  })
 };
